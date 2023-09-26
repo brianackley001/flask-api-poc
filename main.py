cc3c6b803780de8d9125cc2166ea3562a6a676d5
brianackley001 <brian.ch.ackley@gmail.com>
2023-09-26 16:16:49 -0700

    RequestParser for args, required args

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
-names = {
-      "john": {"age": 25, "hobby": "reading"},
-      "jane": {"age": 31, "hobby": "singing"},
-      "tim": {"age": 84, "hobby": "wizard"},
-      "bill": {"age": 22, "hobby": "wyld stallionz"},
-      "ted": {"age": 22, "hobby": "wyld stallionz"}
-      }
+videos = { }
     
-class HelloWorld(Resource):
-  def get(self, name):
-    return names[name]
+class Video(Resource):
+  def get(self, video_id):
+    return videos[video_id]
   
+  def put(self, video_id):
+    args = video_put_args.parse_args()
+    return {video_id: args}
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
   app.run(debug=True)
