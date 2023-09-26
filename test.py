b98f58f4c6cbb41cfa1f94e5b499b7f67aaaa167
brianackley001 <brian.ch.ackley@gmail.com>
2023-09-26 16:25:33 -0700

    PUT request to create new video

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
+response = requests.put(BASE + "video/1", {"name": "Some Video Name","views":25,"likes": 10})
+print(response.json())
+
+input()
+
+response = requests.get(BASE + "video/1")
 print(response.json())
