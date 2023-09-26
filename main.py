420a835a0ac91f8e7a702b350a08672a79351b6e
brianackley001 <brian.ch.ackley@gmail.com>
2023-09-26 16:30:23 -0700

    404 error handling for non existing video id

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse,abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,9 +10,14 @@ video_put_args.add_argument("views", type=int, help="Views of the video is requi
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
 videos = { }
+
+def abort_if_video_id_not_exists(video_id):
+  if video_id not in videos:
+    abort(404, message="Video ID is not valid...")
     
 class Video(Resource):
   def get(self, video_id):
+    abort_if_video_id_not_exists(video_id)
     return videos[video_id]
   
   def put(self, video_id):
