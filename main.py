870ed7de37e26643a59ce2b6b73ecd1a18560c29
brianackley001 <brian.ch.ackley@gmail.com>
2023-09-26 16:04:27 -0700

    Return requested dictionary object on GET request

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+names = {
+      "john": {"age": 25, "hobby": "reading"},
+      "jane": {"age": 31, "hobby": "singing"},
+      "tim": {"age": 84, "hobby": "wizard"},
+      "bill": {"age": 22, "hobby": "wyld stallionz"},
+      "ted": {"age": 22, "hobby": "wyld stallionz"}
+      }
+    
 class HelloWorld(Resource):
-  def get(self, name, test):
-    return {"name": name, "test": test}
+  def get(self, name):
+    return names[name]
   
 
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
   app.run(debug=True)
